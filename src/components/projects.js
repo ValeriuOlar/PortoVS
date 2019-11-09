30e17649da45dea7c8819ee5ea3970dd147d117d
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-09 10:12:03 +0000

    adding Grid, Cell

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,46 @@
 import React, {Component } from 'react';
-import {Tabs, Tab} from 'react-mdl';
+import {Tabs, Tab, Grid, Cell } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props){
         super(props);
         this.state = {activeTab: 0};
     }
+
+    toggleCategories(){
+        if(this.state.activeTab === 0){
+            return(
+                <div>
+                    <h1>This is React.js</h1> 
+                </div>
+            )
+        }else if(this.state.activeTab === 1) {
+            return (
+            <div><h1>This is React</h1></div>
+            )
+        }else if(this.state.activeTab === 2) {
+            return (
+            <div><h1>This is Angular</h1></div>
+            )
+        }else if(this.state.activeTab === 3) {
+            return (
+            <div><h1>This is Vue.JS </h1></div>
+            )
+        }else if(this.state.activeTab === 4) {
+            return (
+            <div><h1>This is MongoBD</h1></div>
+            )
+        }else if(this.state.activeTab === 5) {
+            return (
+            <div><h1>This is Java</h1></div>
+            )
+        }else if(this.state.activeTab === 6) {
+            return (
+            <div><h1>This is Python</h1></div>
+            )
+        }
+    }
+
 
     render() {
         return(
@@ -15,9 +50,13 @@ class Projects extends Component {
                     <Tab>Angular</Tab>
                     <Tab>Vue.JS</Tab>
                     <Tab>MongoBD</Tab>
-
+                    <Tab>Java</Tab>
+                    <Tab>Python</Tab>
                 </Tabs>
-
+            
+            <section className="projects-grid">
+                {this.toggleCategories()}
+            </section>
             </div>
         )
     }
