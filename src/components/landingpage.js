72a237a63a2e148a6a90018a87837b7408b8d9b3
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-09 00:39:40 +0000

    css clean and inline style

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -3,7 +3,9 @@ import React, {Component } from 'react';
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+
+            </div>
         )
     }
 }
