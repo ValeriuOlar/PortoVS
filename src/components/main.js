356088696a7dc8704097fdbb25e446970ac178fc
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-09 00:22:38 +0000

    adding path

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
-import LandingPage from '.landingpage';
 
-const Main = () => {
+import LandingPage from '.landingpage';
+import AboutMe from './aboutme';
+import Contacts from './contacts';
+import Projects from './projects';
+import Resume from './resume';
+
+const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
     </Switch>
-}
+)
 
 export default Main;
