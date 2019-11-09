e95068f47621b7b47af8f569fe929466c185174b
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-09 00:32:41 +0000

    landing page path bug  fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,10 +12,10 @@ function App() {
     <Layout>
         <Header title="VStability" scroll>
             <Navigation>
-                <a href="/">Contact</a>
-                <a href="/">Projects</a>
-                <a href="/">GitHub</a>
-                <a href="/">CV</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Info">
--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 
-import LandingPage from '.landingpage';
+import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contacts from './contacts';
 import Projects from './projects';
@@ -10,6 +10,13 @@ import Resume from './resume';
 const Main = () => (
     <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/landing" component={LandingPage} />
+
+
     </Switch>
 )
 
