84977054c832690a220b5a5a3a5681cb3bd3c7ff
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-10 14:37:22 +0000

    bug fix contacts

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contacts">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Info">
@@ -23,7 +23,7 @@ function App() {
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contacts">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
