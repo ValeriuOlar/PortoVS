0ad9885df62531763a97e77e8734ace2e47e478d
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-11 18:55:16 +0000

    addign avatar style

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,7 +4,14 @@ class Resume extends Component {
     render() {
         return(
             <div>
-                <Cell col={4}>Left Side</Cell>
+                <Cell col={4}>
+                    <div style={{textAlign: 'center'}}>
+                    <img
+                    src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                    alt="avatar"
+                    style={{height: '200px'}}
+                    </div>
+                </Cell>
                 <Cell className="resume-right-col" col={8}>Rights Side</Cell>
             </div>
         )
