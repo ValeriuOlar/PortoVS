4bbe3d8a7fe9187dccc7f2baeea641ef7591e057
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-11 20:32:22 +0000

    adding new experience component and eucation contenent

--- src/components/resume.js
+++ src/components/resume.js
@@ -38,8 +38,29 @@ class Resume extends Component {
                 <Cell className="resume-right-col" col={8}>
                     <h2>Education</h2>
                     <Education
-                    startYear={2012}
+                    startYear={2015}
+                    endYear={2019}
+                    schoolName="NCIRL"
+                    schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                     It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                     and more recently with desktop publishing software like Aldus PageMaker including vers"
                     />
+                     <Education
+                    startYear={2015}
+                    endYear={2019}
+                    schoolName="NCIRL"
+                    schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                     It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                     and more recently with desktop publishing software like Aldus PageMaker including vers"
+                    />
+                    <hr style={{borderTop: '3p solid #e22947'}}/>
+                    <h2>Experiece</h2>
                 </Cell>
              </Grid>
             </div>
