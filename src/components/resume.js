0b1ef349ada90571a7d2e82d5a673753b1645465
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-11 18:56:32 +0000

    ading grid and fix

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,15 +4,18 @@ class Resume extends Component {
     render() {
         return(
             <div>
+                <Grid>
                 <Cell col={4}>
                     <div style={{textAlign: 'center'}}>
                     <img
                     src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
                     alt="avatar"
                     style={{height: '200px'}}
+                    />
                     </div>
                 </Cell>
                 <Cell className="resume-right-col" col={8}>Rights Side</Cell>
+                </Grid>
             </div>
         )
     }
