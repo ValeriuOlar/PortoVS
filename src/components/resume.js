d459d86b2ea062adb7ba796d4d5e066f93156e9a
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-11-11 19:05:09 +0000

    resume structure

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,18 +4,33 @@ class Resume extends Component {
     render() {
         return(
             <div>
-                <Grid>
+             <Grid>
                 <Cell col={4}>
-                    <div style={{textAlign: 'center'}}>
+                   <div style={{textAlign: 'center'}}>
                     <img
                     src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
                     alt="avatar"
                     style={{height: '200px'}}
                     />
                     </div>
+
+                    <h2 style={{paddingTop:'2em'}}>Valeriu Olariu</h2>
+                    <h4 style={{color: 'grey'}}>Programer</h4>
+                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                         when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                         It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                         and more recently with desktop publishing software like Aldus PageMaker including vers</p>
+                         <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+                        <h5>Address</h5>
+                        <p>Lorem Ipsum is simply dummy text</p>
+                        <h5>Phone</h5>
+                        <p>(231) 1314 123 </p>
                 </Cell>
                 <Cell className="resume-right-col" col={8}>Rights Side</Cell>
-                </Grid>
+             </Grid>
             </div>
         )
     }
